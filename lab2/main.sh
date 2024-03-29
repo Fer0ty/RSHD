98d56158998bb1218ac220cf51b19d07f62a91b3
artemiy <Ntvrfntvrf20031905!>
2024-03-29 13:48:48 +0300

    ooooffff

--- lab2/main.sh
+++ lab2/main.sh
@@ -4,7 +4,7 @@ initdb -E KOI8-R --locale=ru_RU.KOI8-R
 # настройка
 rm $PGDATA/postgresql.conf
 cp config/postgresql.conf $PGDATA/postgresql.conf
-
+echo "host                all   all             0.0.0.0/0           md5" >> $PGDATA/pg_hba.conf
 # запуск
 pg_ctl -D $PGDATA -l $PGDATA/start_serverlog.log start
 sleep 3
--- lab2/sql/init.sql
+++ lab2/sql/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLESPACE ycv6 LOCATION '/var/db/postgres0/ycv6';
 CREATE TABLESPACE hxb42 LOCATION '/var/db/postgres0/hxb42';
 
--- создание базу данных
+-- создание базы данных
 CREATE DATABASE busyyellowuser TEMPLATE template1;
 
